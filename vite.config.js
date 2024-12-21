81f7e00a44a1cc626300513a381a3b995d797591
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-12-21 21:39:30 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            "main.js": path.resolve(
+                __dirname,
+                "public/front/assets/js/main.js"
+            ),
+            "app.css": path.resolve(__dirname, "resources/css/app.css"),
+        },
+    },
+    optimizeDeps: {
+        include: ["main.js", "app.css"],
+    },
 });
