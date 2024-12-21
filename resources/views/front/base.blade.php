bf8cf0da0466932f2d63f8d743e3ec877b7a825f
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-12-21 22:24:26 +0300

    fşx

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html lang="tr">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <title>Morning Blog</title>
+
+    @vite('resources/css/app.css')
+
+</head>
+
+<body>
+
+    @include('front.partials.header')
+
+    @yield('content')
+
+    @include('front.partials.footer')
+
+    @include('front.partials.script')
+
+</body>
+
+</html>
---
+++ resources/views/front/partials/script.blade.php
@@ -0,0 +1,3 @@
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+<script src="{{ asset('front/assets/js/main.js') }}"></script>
