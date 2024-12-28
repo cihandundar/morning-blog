2ec727f0c9a068afecfc58ccead365c0f98d53e3
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-12-28 20:41:16 +0300

    featured bölümü kodlandı

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -5,4 +5,5 @@
 @section('content')
     @include('front.partials.sections.hero')
     @include('front.partials.sections.latest-news')
+    @include('front.partials.sections.featured')
 @endsection
---
+++ resources/views/front/partials/sections/featured.blade.php
@@ -0,0 +1,88 @@
+<section class="py-[90px]">
+    <div class="container max-w-screen-xl mx-auto">
+        <div class="lg:text-4xl pb-5 border-b-2 border-gray-800">
+            Featured
+        </div>
+        <div class="grid grid-cols-1 lg:grid-cols-12 gap-10 pt-5">
+            <div class="lg:col-span-6 pt-4 ">
+                <div class="flex flex-col gap-5">
+                    <a href="#" class="flex items-center gap-5 ">
+                        <div class="flex flex-col justify-between gap-2">
+                            <div class="text-2xl pb-2">Virtual reality headsets take gaming to the next level</div>
+                            <div class="text-sm capitalize font-thin">Technology - May 13, 2024</div>
+                        </div>
+                        <img src="{{ asset('front/assets/images/featured1.jpg') }}" alt="featured image"
+                            class=" w-[40%] object-cover">
+                    </a>
+                    <a href="#" class="flex items-center gap-5 ">
+                        <div class="flex flex-col justify-between gap-2">
+                            <div class="text-2xl pb-2">Global summit convenes world leaders to address climate emergency
+                            </div>
+                            <div class="text-sm capitalize font-thin">World News - May 13, 2024</div>
+                        </div>
+                        <img src="{{ asset('front/assets/images/featured2.jpg') }}" alt="featured image"
+                            class=" w-[40%] object-cover">
+                    </a>
+                    <a href="#" class="flex items-center gap-5 ">
+                        <div class="flex flex-col justify-between gap-2">
+                            <div class="text-2xl pb-2">Exclusive interview with award-winning author on latest book
+                                release</div>
+                            <div class="text-sm capitalize font-thin">Technology - May 13, 2024</div>
+                        </div>
+                        <img src="{{ asset('front/assets/images/featured3.jpg') }}" alt="featured image"
+                            class=" w-[40%] object-cover">
+                    </a>
+                    <a href="#" class="flex items-center gap-5 ">
+                        <div class="flex flex-col justify-between gap-2">
+                            <div class="text-2xl pb-2">Highlighting importance of well-being</div>
+                            <div class="text-sm capitalize font-thin">Sports - May 13, 2024</div>
+                        </div>
+                        <img src="{{ asset('front/assets/images/featured4.jpg') }}" alt="featured image"
+                            class=" w-[40%] object-cover">
+                    </a>
+                </div>
+            </div>
+
+            <div class="lg:col-span-6 pt-4">
+                <div class="flex flex-col gap-5">
+                    <a href="#" class="flex items-center gap-5 ">
+                        <div class="flex flex-col justify-between gap-2">
+                            <div class="text-2xl pb-2">5G rollout accelerates how connectivity next-gen will transform
+                                industries</div>
+                            <div class="text-sm capitalize font-thin">Technology - May 13, 2024</div>
+                        </div>
+                        <img src="{{ asset('front/assets/images/featured5.jpg') }}" alt="featured image"
+                            class=" w-[40%] object-cover">
+                    </a>
+                    <a href="#" class="flex items-center gap-5 ">
+                        <div class="flex flex-col justify-between gap-2">
+                            <div class="text-2xl pb-2">Athlete achieves historic win at world championships
+                            </div>
+                            <div class="text-sm capitalize font-thin">Sports - May 13, 2024</div>
+                        </div>
+                        <img src="{{ asset('front/assets/images/featured6.jpg') }}" alt="featured image"
+                            class=" w-[40%] object-cover">
+                    </a>
+                    <a href="#" class="flex items-center gap-5 ">
+                        <div class="flex flex-col justify-between gap-2">
+                            <div class="text-2xl pb-2">Local community rallies to support homeless</div>
+                            <div class="text-sm capitalize font-thin">World News - May 13, 2024</div>
+                        </div>
+                        <img src="{{ asset('front/assets/images/featured7.jpg') }}" alt="featured image"
+                            class=" w-[40%] object-cover">
+                    </a>
+                    <a href="#" class="flex items-center gap-5 ">
+                        <div class="flex flex-col justify-between gap-2">
+                            <div class="text-2xl pb-2">Unraveling the marvels of life sciences, genetics, evolution, and
+                                ecological</div>
+                            <div class="text-sm capitalize font-thin">Health - May 13, 2024</div>
+                        </div>
+                        <img src="{{ asset('front/assets/images/featured8.jpg') }}" alt="featured image"
+                            class=" w-[40%] object-cover">
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
