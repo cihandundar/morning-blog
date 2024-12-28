aac2a714b29ab17102b94d820e0f5c33bc05c7f4
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-12-29 01:00:59 +0300

    footer kodlandı

--- resources/views/front/partials/footer.blade.php
+++ resources/views/front/partials/footer.blade.php
@@ -1,1 +1,81 @@
+<footer class="py-[90px] bg-black">
+    <div class="container max-w-screen-xl mx-auto">
+        <div class="grid grid-cols-1 lg:grid-cols-12 gap-20">
+            <div class="lg:col-span-4 flex flex-col gap-5">
+                <a class="flex flex-col" href="/">@include('front.partials.svg.logo')
+                    <span class="text-xs text-white">Inspiring Minds, Igniting Change.</span>
+                </a>
 
+                <div class="flex flex-col gap-2 text-white">
+                    <span>Subscribe to our newsletter.</span>
+                    <div class="flex items-center gap-3">
+                        <form action="#" method="#">
+                            <input class="py-2 pl-5 pr-24 rounded-3xl text-sm text-black" type="text"
+                                placeholder="Enter your email.." required>
+                            <button class="bg-red-600 py-2 px-3 rounded-full"><i
+                                    class="fa-solid fa-arrow-right"></i></button>
+                        </form>
+                    </div>
+                    <span class="text-xs text-white">From breaking news to thought-provoking opinion pieces, our
+                        newsletter keeps you informed &
+                        engaged.</span>
+                </div>
+            </div>
+            <div class="lg:col-span-8">
+                <div class="lg:flex lg:justify-between flex-wrap">
+                    <div class="flex flex-col gap-2">
+                        <span class="text-white uppercase">Pages</span>
+                        <ul class="flex flex-col gap-3 text-gray-400">
+                            <li><a class="hover:text-red-600" href="#">About us</a></li>
+                            <li><a class="hover:text-red-600" href="#">News</a></li>
+                            <li><a class="hover:text-red-600" href="#">Authors</a></li>
+                            <li><a class="hover:text-red-600" href="#">Subscription</a></li>
+                            <li><a class="hover:text-red-600" href="#">Contact us</a></li>
+                        </ul>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <span class="text-white uppercase">Categories</span>
+                        <ul class="flex flex-col gap-3 text-gray-400">
+                            <li><a class="hover:text-red-600" href="#">World News</a></li>
+                            <li><a class="hover:text-red-600" href="#">Politics</a></li>
+                            <li><a class="hover:text-red-600" href="#">Health</a></li>
+                            <li><a class="hover:text-red-600" href="#">Sports</a></li>
+                            <li><a class="hover:text-red-600" href="#">Technology</a></li>
+                            <li><a class="hover:text-red-600" href="#">Business</a></li>
+                        </ul>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <span class="text-white uppercase">Resources</span>
+                        <ul class="flex flex-col gap-3 text-gray-400">
+                            <li><a class="hover:text-red-600" href="#">Privacy Policy</a></li>
+                            <li><a class="hover:text-red-600" href="#">Advertise</a></li>
+                            <li><a class="hover:text-red-600" href="#">Terms & Conditions</a></li>
+                            <li><a class="hover:text-red-600" href="#">License</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul class="text-white flex flex-col gap-3">
+                            <li
+                                class="w-[40px] h-[40px] flex items-center justify-center border-2 border-gray-400 rounded-full hover:bg-white hover:text-black transition-all duration-300">
+                                <a class=" " href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                            </li>
+                            <li
+                                class="w-[40px] h-[40px] flex items-center justify-center border-2 border-gray-400 rounded-full hover:bg-white hover:text-black transition-all duration-300">
+                                <a class=" " href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                            </li>
+                            <li
+                                class="w-[40px] h-[40px] flex items-center justify-center border-2 border-gray-400 rounded-full hover:bg-white hover:text-black transition-all duration-300">
+                                <a class=" " href="#"><i class="fa-brands fa-youtube"></i></a>
+                            </li>
+                            <li
+                                class="w-[40px] h-[40px] flex items-center justify-center border-2 border-gray-400 rounded-full hover:bg-white hover:text-black transition-all duration-300">
+                                <a class=" " href="#"><i class="fa-brands fa-instagram"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+</footer>
